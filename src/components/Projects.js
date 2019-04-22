faa87586e2374b92432d5ef051ee5a10f2c81ab7
Enrico <hdpjenrico@gmail.com>
2019-04-22 02:06:33 -0300

    Adicionei os Repositórios do git

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react'
-import Axios from 'axios'
+import React, { Component } from 'react';
+import { fetchRepos } from '../services/repo-api';
+const ReposList = props => {
+  return (<ul>
+    {props.repos.map(repo => (
+      <li key={repo.id}>{repo.name}</li>
+    ))}
+  </ul>)
+}
+
 export class Work extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      repos: [],
+      username: ''
+    }
+    this.changeHandler = this.changeHandler.bind(this)
+    this.submitHandler = this.submitHandler.bind(this)
+  }
+
+  //
+  changeHandler(ev) {
+    this.setState({ username: ev.target.value })
+  }
+
+  //
+  submitHandler(ev) {
+    ev.preventDefault()
+    fetchRepos(this.state.username).then(res => this.setState({ repos: res.data }))
+  }
+
+  //
   render() {
     return (
       <div>
-          <h1>Ma Projects faggot</h1>
+        <h1>Repos</h1>
+        <form action="#" onSubmit={this.submitHandler}>
+          <input
+            onChange={this.changeHandler}
+            style={{width: '250px'}}
+            type="search"
+            placeholder="Informe usuário e tecle ENTER"/>
+        </form>
+        <ReposList repos={this.state.repos}></ReposList>
       </div>
     )
   }
 }
+
+
 
 export default Work
---
+++ src/services/repo-api.js
@@ -0,0 +1,2 @@
+import { http } from './http';
+export const fetchRepos = user => http.get(`users/${user}/repos`);
